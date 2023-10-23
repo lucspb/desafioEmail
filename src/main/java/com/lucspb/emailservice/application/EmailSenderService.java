3df37d1e4488d7b714a6c77bd2d205b2986f8389
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-10-23 09:17:11 -0300

    criando estrutura de envio de emails

--- src/main/java/com/lucspb/emailservice/application/EmailSenderService.java
+++ src/main/java/com/lucspb/emailservice/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.lucspb.emailservice.application;
 import com.lucspb.emailservice.adapters.EmailSenderGateway;
 import com.lucspb.emailservice.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
